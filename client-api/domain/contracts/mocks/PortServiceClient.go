d90b31ded43a729950d8c3adec1e44e28b3fe836
Daniel Kock <danielk.kock@gmail.com>
2023-09-18 14:30:37 -0300

    test: create stream open and close methods in grpc client

--- client-api/domain/contracts/mocks/PortServiceClient.go
+++ client-api/domain/contracts/mocks/PortServiceClient.go
@@ -41,30 +41,70 @@ func (_m *PortServiceClient) FindByID(ctx context.Context, id string) (*port.Por
 	return r0, r1
 }
 
-// StreamCreate provides a mock function with given fields: ctx, ports
-func (_m *PortServiceClient) StreamCreate(ctx context.Context, ports []*port.Port) (*port.CreateResponse, error) {
-	ret := _m.Called(ctx, ports)
+// StreamClose provides a mock function with given fields: ctx, stream
+func (_m *PortServiceClient) StreamClose(ctx context.Context, stream port.PortService_CreateClient) (*port.CreateResponse, error) {
+	ret := _m.Called(ctx, stream)
 
 	var r0 *port.CreateResponse
 	var r1 error
-	if rf, ok := ret.Get(0).(func(context.Context, []*port.Port) (*port.CreateResponse, error)); ok {
-		return rf(ctx, ports)
+	if rf, ok := ret.Get(0).(func(context.Context, port.PortService_CreateClient) (*port.CreateResponse, error)); ok {
+		return rf(ctx, stream)
 	}
-	if rf, ok := ret.Get(0).(func(context.Context, []*port.Port) *port.CreateResponse); ok {
-		r0 = rf(ctx, ports)
+	if rf, ok := ret.Get(0).(func(context.Context, port.PortService_CreateClient) *port.CreateResponse); ok {
+		r0 = rf(ctx, stream)
 	} else {
 		if ret.Get(0) != nil {
 			r0 = ret.Get(0).(*port.CreateResponse)
 		}
 	}
 
-	if rf, ok := ret.Get(1).(func(context.Context, []*port.Port) error); ok {
-		r1 = rf(ctx, ports)
+	if rf, ok := ret.Get(1).(func(context.Context, port.PortService_CreateClient) error); ok {
+		r1 = rf(ctx, stream)
 	} else {
 		r1 = ret.Error(1)
 	}
 
 	return r0, r1
+}
+
+// StreamCreate provides a mock function with given fields: ctx
+func (_m *PortServiceClient) StreamCreate(ctx context.Context) (port.PortService_CreateClient, error) {
+	ret := _m.Called(ctx)
+
+	var r0 port.PortService_CreateClient
+	var r1 error
+	if rf, ok := ret.Get(0).(func(context.Context) (port.PortService_CreateClient, error)); ok {
+		return rf(ctx)
+	}
+	if rf, ok := ret.Get(0).(func(context.Context) port.PortService_CreateClient); ok {
+		r0 = rf(ctx)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).(port.PortService_CreateClient)
+		}
+	}
+
+	if rf, ok := ret.Get(1).(func(context.Context) error); ok {
+		r1 = rf(ctx)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// StreamSendPortFile provides a mock function with given fields: stream, _a1
+func (_m *PortServiceClient) StreamSendPortFile(stream port.PortService_CreateClient, _a1 *port.Port) error {
+	ret := _m.Called(stream, _a1)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(port.PortService_CreateClient, *port.Port) error); ok {
+		r0 = rf(stream, _a1)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
 }
 
 // NewPortServiceClient creates a new instance of PortServiceClient. It also registers a testing interface on the mock and a cleanup function to assert the mocks expectations.
